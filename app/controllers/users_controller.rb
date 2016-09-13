e37b8d2b119ce5e597b6f238d3238b28eb9b769d
David Ladowitz <david@ladowitz.com>
2016-09-12 18:11:58 -0700

    addings specs and feature for user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
     render json: @users
   end
 
+  def create
+    user = User.new user_params
+    if user.save
+      render json: user, status: 201
+    else
+      render json: "Sorry, we couldn't create a user from those params", status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email_address, :birthdate)
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,21 @@ require 'rails_helper'
 
 describe UsersController do
   describe "POST #create" do
+    subject { post :create, user: { email_address: "aiko@gmail.com", birthdate: Date.parse("2006-01-01") }}
 
+    it "creates a new user in the database" do
+      expect{ subject }.to change{ User.count }.by 1
+    end
+
+    it "returns status code of 201" do
+      subject
+      expect(response.status).to eq 201
+    end
+
+    it "returns the created user" do
+      subject
+      expect(JSON.parse(response.body)["email_address"]).to eq "aiko@gmail.com"
+    end
   end
 
   describe "GET #index" do
@@ -11,17 +25,16 @@ describe UsersController do
       User.create email_address: "aiko@gmail.com", birthdate: Date.parse("2006-01-01")
       User.create email_address: "dusty@gmail.com", birthdate: Date.parse("2006-02-01")
       subject
+      @users = JSON.parse response.body
     end
 
     it "returns all the users in the database" do
-      users = JSON.parse response.body
-      expect(users.count).to eq 2
+      expect(@users.count).to eq 2
     end
 
     it "returns a list of users" do
-      users = JSON.parse response.body
-      expect(users.first["email_address"]).to be_present
-      expect(users.first["birthdate"]).to be_present
+      expect(@users.first["email_address"]).to be_present
+      expect(@users.first["birthdate"]).to be_present
     end
   end
 end
